e9b6afc8425b4667ef535360c8f223e63bb0d197
lxyoutlook <34645156+lxyoutlook@users.noreply.github.com>
2018-04-05 20:25:14 +0800

    Add files via upload

---
+++ RGBLED_new.py
@@ -0,0 +1,85 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+
+import RPi.GPIO as GPIO
+import time
+
+class RGBLedSetter:
+    def __init__(self):
+        self.R_LED = 11  # GPIO17
+        self.G_LED = 12  # GPIO18
+        self.B_LED = 13  # GPIO27
+
+        GPIO.setwarnings(False)        # Disable warnings
+        GPIO.setmode(GPIO.BOARD)       # Numbers GPIOs by physical location
+        GPIO.setup(self.R_LED, GPIO.OUT)  # Set pins' mode is output
+        GPIO.output(self.R_LED, GPIO.HIGH)  # Set pins to high(GND) to off led
+        GPIO.setup(self.G_LED, GPIO.OUT)
+        GPIO.output(self.G_LED, GPIO.HIGH)
+        GPIO.setup(self.B_LED, GPIO.OUT)
+        GPIO.output(self.B_LED, GPIO.HIGH)
+
+        self.p_R = GPIO.PWM(self.R_LED, 2000)  # set Frequece to 2kHz
+        self.p_G = GPIO.PWM(self.G_LED, 2000)
+        self.p_B = GPIO.PWM(self.B_LED, 2000)
+
+        self.p_R.start(100)      # Initial duty Cycle = 100(leds off)
+        self.p_G.start(100)
+        self.p_B.start(100)
+
+
+    def R_Led_on(self):
+        self.p_R.ChangeDutyCycle(0)
+        self.p_G.ChangeDutyCycle(100)
+        self.p_B.ChangeDutyCycle(100)
+        time.sleep(0.4)
+
+    def G_Led_on(self):
+        self.p_R.ChangeDutyCycle(100)
+        self.p_G.ChangeDutyCycle(0)
+        self.p_B.ChangeDutyCycle(100)
+        time.sleep(0.4)
+
+    def B_Led_on(self):
+        self.p_R.ChangeDutyCycle(100)
+        self.p_G.ChangeDutyCycle(100)
+        self.p_B.ChangeDutyCycle(0)
+        time.sleep(0.4)
+
+    def all_Led_off(self):
+        self.p_R.ChangeDutyCycle(100)
+        self.p_G.ChangeDutyCycle(100)
+        self.p_B.ChangeDutyCycle(100)
+        time.sleep(0.4)
+        GPIO.cleanup
+
+
+if __name__ == "__main__":
+    prompt_msg = \
+        u"Please input following instrcutions:\n"\
+        u"R or r:-----RED LED on\n"\
+        u"G or g:-----GREEN LED on\n"\
+        u"B or b:-----BLUE LED on\n"\
+        u"O or o:-----LED off\n"
+    myLedSetter = RGBLedSetter()
+    myLedSetter.all_Led_off()
+
+    try:
+        while True:
+            received_msg = raw_input(prompt_msg)
+            if received_msg.upper() == u'R':
+                myLedSetter.R_Led_on()
+            elif received_msg.upper() == u'G':
+                myLedSetter.G_Led_on()
+            elif received_msg.upper() == u'B':
+                myLedSetter.B_Led_on()
+            elif received_msg.upper() == u'O':
+                myLedSetter.all_Led_off()
+            else:
+                pass
+
+    except KeyboardInterrupt:
+        myLedSetter.p_R.stop()
+        myLedSetter.p_G.stop()
+        myLedSetter.p_B.stop()
+        GPIO.cleanup()
